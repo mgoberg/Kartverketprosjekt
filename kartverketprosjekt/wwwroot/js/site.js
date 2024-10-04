27be54cf49f474f19607da05fbbaa2359f55c857
SimonPortillo <simon.portillo@outlook.com>
2024-10-04 15:58:43 +0200

    la til layout html og css

--- kartverketprosjekt/wwwroot/js/site.js
+++ kartverketprosjekt/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//SETUP
 
-// Write your JavaScript code.
+// enables fading in of body and navbar on initial load, remove styles on navbar and body if page appears blank
+window.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('navbar').style.top = '0';
+    document.querySelector('body').style.opacity = '1';
+});
+
+// function for darkmode
+document.getElementById('darkModeToggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+});
